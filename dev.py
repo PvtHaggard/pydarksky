7eff942f66a8cdf23c9122328ecf026bf916e90e
PvtHaggard <pvthaggard@gmail.com>
2017-07-30 19:12:46 +0930

    Removed and reset my API key

--- dev.py
+++ dev.py
@@ -10,8 +10,11 @@ log = logging.getLogger(__name__)
 
 
 def main():
-    darksky = DarkSkyPy("677540efddd5db9d74337dc6a2da26e6")
-    weather = darksky.weather(-34.9286, 138.5999)
+    darksky = DarkSkyPy("0" * 32)
+    # weather = darksky.weather(-34.9286, 138.5999)
+    darksky.latitude = 12.3
+    darksky.longitude = -32.1
+    print(darksky.url)
     pass
 
 
