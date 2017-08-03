a49e36b8abc05f41e943d55d5e7142262ead941c
PvtHaggard <pvthaggard@gmail.com>
2017-08-04 01:40:40 +0930

    Setup weather.py tests

---
+++ tests/test_weather.py
@@ -0,0 +1,25 @@
+import unittest
+import logging
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from darkskypy import DarkSkyPy
+from darkskypy import Weather
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("darkskypy")
+
+assert os.path.isfile("testdata.json"), "JSON file missing can not run tests"
+with open("testdata.json") as f:
+    test_json = f.read()
+
+
+class TestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
