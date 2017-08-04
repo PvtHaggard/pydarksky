d5ad7f879b9781c52b1058b50e97f8d4d4f5fbda
PvtHaggard <pvthaggard@gmail.com>
2017-08-04 17:09:56 +0930

    Travis-ci debug

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -11,7 +11,8 @@ from darkskypy import Weather
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("darkskypy")
 
-assert os.path.isfile("testdata.json"), "JSON file missing can not run tests"
+# assert os.path.isfile("testdata.json"), "JSON file missing can not run tests"
+logging.info(os.listdir(os.curdir))
 with open("testdata.json") as f:
     test_json = f.read()
 
