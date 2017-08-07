da1adff25babab83f79d01514b6fe6a07104e655
Jordan Stocker <pvthaggard@gmail.com>
2017-08-07 11:11:34 +0930

    Revert "Removed test json import"
    This reverts commit 73c639c5bdefd09b795dce622d6b083bde85c737.

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -11,10 +11,16 @@ from darkskypy import Weather
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("darkskypy")
 
+tmp_path = os.path.join(sys.path[0], 'tests')
+tmp_path = os.path.join(tmp_path, "testdata.json")
+assert os.path.isfile(tmp_path), "JSON file missing can not run tests"
+with open(tmp_path) as f:
+    test_json = f.read()
+
 
 class TestCase(unittest.TestCase):
     def test_weather_init(self):
-        pass
+        Weather(test_json)
 
 
 if __name__ == '__main__':
