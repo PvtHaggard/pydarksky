c9e9fd306075fbc6a24aada2bca7fe36ba4c0a76
PvtHaggard <pvthaggard@gmail.com>
2017-08-30 23:38:58 +0930

    Data classes now set properties dynamically

--- pydarksky/weather.py
+++ pydarksky/weather.py
@@ -15,199 +15,131 @@ log = logging.getLogger("pydarksky")
 
 
 class Weather:
+    """
+    Do not assume the existence of any property unless stated otherwise.
+
+    :param str json_raw:
+
+    :var dict json: [Required] JSON data returned by the Dark Sky API
+    :var float latitude: [Required] The requested latitude. Maybe different from the value passed to DarkSky class.
+    :var float longitude: [Required] The requested longitude. Maybe different from the value passed to DarkSky class.
+    :var str timezone: [Required] The IANA timezone name for the requested location. This is used for text summaries and for determining when hourly and daily data block objects begin.
+    :var float offset: [Deprecated] The current timezone offset in hours. (Use of this property will almost certainly result in Daylight Saving Time bugs. Please use timezone, instead.)
+
+    :var Currently currently: A class containing the current weather conditions at the requested location.
+
+    :var list[Daily] daily: A class containing the current weather conditions at the requested location.
+    :var str daily_summary: A human-readable summary of the daily data block.
+    :var str daily_icon: A machine-readable text summary of the daily data block.
+
+    :var list[Hourly] hourly: A class containing the current weather conditions day-by-day for the next week.
+    :var str hourly_summary: A human-readable summary of the hourly data block.
+    :var str hourly_icon: A machine-readable text summary of the daily data block.
+
+    :var list[Minutely] minutely: A class containing the current weather conditions minute-by-minute for the next hour.
+    :var str minutely_summary: A human-readable summary of the minutely data block.
+    :var str minutely_icon: A machine-readable text summary of the daily data block.
+
+    :var Alert alerts: An alerts array, which, if present, contains any severe weather alerts pertinent to the requested location.
+    """
+
     def __init__(self, json_raw):
         # type:(str) -> None
         self.json = json.loads(json_raw)
+        self.latitude = self.json["latitude"]
+        self.longitude = self.json["longitude"]
+        self.timezone = self.json["timezone"]
+        self.offset = self.json["offset"]
 
-    @property
-    def latitude(self):
-        # type:() -> float
-        return self.json.get("latitude")
-
-    @property
-    def longitude(self):
-        # type:() -> float
-        return self.json.get("longitude")
-
-    @property
-    def timezone(self):
-        # type:() -> str
-        return self.json.get("timezone")
-
-    @property
-    def offset(self):
-        # type:() -> float
-        return self.json.get("offset")
-
-    @property
-    def currently(self):
-        # type:() -> Currently
         if "currently" in self.json:
-            return Currently(self.json["currently"], self)
+            self.currently = Currently(self.json["currently"], self)
 
-    @property
-    def daily(self):
-        # type:() -> list
         if "daily" in self.json:
-            daily = []
+            self.daily = []
+            if self.json["daily"].get("summary") is not None:
+                self.daily_summary = self.json["daily"].get("summary")
+            if self.json["daily"].get("icon") is not None:
+                self.daily_icon = self.json["daily"].get("icon")
             for data in self.json["daily"]["data"]:
-                daily.append(Daily(data, self))
-            return daily
+                self.daily.append(Daily(data, self))
 
-    @property
-    def daily_summary(self):
-        # type:() -> str
-        if "daily" in self.json:
-            return self.json["daily"].get("summary")
-
-    @property
-    def daily_icon(self):
-        # type:() -> str
-        if "daily" in self.json:
-            return self.json["daily"].get("icon")
-
-    @property
-    def hourly(self):
-        # type:() -> list
         if "hourly" in self.json:
-            hourly = []
+            self.hourly = []
+            if self.json["hourly"].get("summary") is not None:
+                self.hourly_summary = self.json["hourly"].get("summary")
+            if self.json["hourly"].get("icon") is not None:
+                self.hourly_icon = self.json["hourly"].get("icon")
             for data in self.json["hourly"]["data"]:
-                hourly.append(Hourly(data, self))
-            return hourly
+                self.hourly.append(Hourly(data, self))
 
-    @property
-    def hourly_summary(self):
-        # type:() -> str
-        if "hourly" in self.json:
-            return self.json["hourly"].get("summary")
-
-    @property
-    def hourly_icon(self):
-        # type:() -> str
-        if "hourly" in self.json:
-            return self.json["hourly"].get("icon")
-
-    @property
-    def minutely(self):
-        # type:() -> list
         if "minutely" in self.json:
-            minutely = []
+            self.minutely = []
+            if self.json["minutely"].get("summary") is not None:
+                self.minutely_summary = self.json["minutely"].get("summary")
+            if self.json["minutely"].get("icon") is not None:
+                self.minutely_icon = self.json["minutely"].get("icon")
             for data in self.json["minutely"]["data"]:
-                minutely.append(Minutely(data, self))
-            return minutely
+                self.minutely.append(Minutely(data, self))
 
-    @property
-    def minutely_summary(self):
-        # type:() -> str
-        if "minutely" in self.json:
-            return self.json["minutely"].get("summary")
-
-    @property
-    def minutely_icon(self):
-        # type:() -> str
-        if "minutely" in self.json:
-            return self.json["minutely"].get("icon")
-
-    @property
-    def alerts(self):
-        # type:() -> Alerts
         if "alerts" in self.json:
-            return Alerts(self.json["alerts"], self)
+            self.alerts = Alerts(self.json["alerts"], self)
 
 
 class WeatherData:
     def __init__(self, data, parent=None):
         # type:(dict, Weather) -> None
-        self.cloud_cover = data.get("cloudCover", None)
-        self.dew_point = data.get("dewPoint", None)
-        self.humidity = data.get("humidity", None)
-        self.icon = data.get("icon", None)
-        self.ozone = data.get("ozone", None)
-        self.pressure = data.get("pressure", None)
-        self.summary = data.get("summary", None)
-        self.time = data.get("time", None)
-        self.uv_index = data.get("uvIndex", None)
-        self.visibility = data.get("visibility", None)
-        self.weather = parent
-        self.wind_bearing = data.get("windBearing", None)
-        self.wind_gust = data.get("windGust", None)
-        self.wind_speed = data.get("windSpeed", None)
+        self.parent = parent
+        for field, value in data.items():
+            setattr(self, field, value)
 
 
 class Currently(WeatherData):
     def __init__(self, data, parent=None):
         # type:(dict, Weather) -> None
         super().__init__(data, parent)
-        self.apparent_temperature = data.get("apparentTemperature", None)
-        self.precipitation_intensity = data.get("precipIntensity", None)
-        self.precipitation_probability = data.get("precipProbability", None)
-        self.precipitation_Type = data.get("precipType", None)
-        self.storm_bearing = data.get("nearestStormBearing")
-        self.storm_distance = data.get("nearestStormDistance")
-        self.temperature = data.get("temperature", None)
 
 
 class Daily(WeatherData):
     def __init__(self, data, parent=None):
         # type:(dict, Weather) -> None
         super().__init__(data, parent)
-        self.apparent_temperature_max = data.get("apparentTemperatureMax", None)
-        self.apparent_temperature_max_time = data.get("apparentTemperatureMaxTime", None)
-        self.apparent_temperature_min = data.get("apparentTemperatureMin", None)
-        self.apparent_temperature_min_time = data.get("apparentTemperatureMinTime", None)
-        self.moon_phase = data.get("moonPhase", None)
-        self.precipitation_accumulation = data.get("precipAccumulation", None)
-        self.precipitation_intensity = data.get("precipIntensity", None)
-        self.precipitation_intensity_max = data.get("precipIntensityMax", None)
-        self.precipitation_intensity_max_time = data.get("precipIntensityMaxTime", None)
-        self.precipitation_probability = data.get("precipProbability", None)
-        self.precipitation_type = data.get("precipType", None)
-        self.sunrise_time = data.get("sunriseTime", None)
-        self.sunset_time = data.get("sunsetTime", None)
-        self.temperature = data.get("temperature", None)
-        self.temperature_max = data.get("temperatureMax", None)
-        self.temperature_max_time = data.get("temperatureMaxTime", None)
-        self.temperature_min = data.get("temperatureMin", None)
-        self.temperature_min_time = data.get("temperatureMinTime", None)
-        self.uv_index_time = data.get("uvIndexTime", None)
-        self.wind_gust_time = data.get("windGustTime", None)
 
 
 class Hourly(WeatherData):
     def __init__(self, data, parent=None):
         # type:(dict, Weather) -> None
         super().__init__(data, parent)
-        self.apparent_temperature = data.get("apparentTemperature", None)
-        self.precipitation_accumulation = data.get("precipAccumulation", None)
-        self.precipitation_intensity = data.get("precipIntensity", None)
-        self.precipitation_probability = data.get("precipProbability", None)
-        self.precipitation_Type = data.get("precipType", None)
-        self.storm_bearing = data.get("nearestStormBearing")
-        self.storm_distance = data.get("nearestStormDistance")
-        self.temperature = data.get("temperature", None)
 
 
 class Minutely(WeatherData):
     def __init__(self, data, parent=None):
         # type:(dict, Weather) -> None
         super().__init__(data, parent)
-        self.apparent_temperature = data.get("apparentTemperature", None)
-        self.precipitation_accumulation = data.get("precipAccumulation", None)
-        self.precipitation_intensity = data.get("precipIntensity", None)
-        self.precipitation_probability = data.get("precipProbability", None)
-        self.precipitation_Type = data.get("precipType", None)
-        self.storm_bearing = data.get("nearestStormBearing")
-        self.storm_distance = data.get("nearestStormDistance")
 
 
 class Alerts:
+    """
+    :var str description: A detailed description of the alert.
+    :var int expires: The UNIX time at which the alert will expire.
+    :var str regions: An array of strings representing the names of the regions covered by this weather alert.
+    :var str severity: The severity of the weather alert, will be one of the following values:
+
+
+        * **advisory** (an individual should be aware of potentially severe weather)
+        * **watch** (an individual should prepare for potentially severe weather)
+        * **warning** (an individual should take immediate action to protect themselves and others from potentially severe weather).
+
+    :var int time: The UNIX time at which the alert was issued.
+    :var str title: A brief description of the alert.
+    :var str uri: An HTTP(S) URI that one may refer to for detailed information about the alert.
+    """
     def __init__(self, data, parent=None):
         # type:(dict, Weather) -> None
         self.weather = parent
-        self.description = data.get("description", None)
-        self.expires = data.get("expires", None)
-        self.regions = data.get("regions", None)
-        self.severity = data.get("severity", None)
-        self.time = data.get("time", None)
-        self.title = data.get("title", None)
-        self.uri = data.get("uri", None)
+        self.description = data["description"]
+        self.expires = data["expires"]
+        self.regions = data["regions"]
+        self.severity = data["severity"]
+        self.time = data["time"]
+        self.title = data["title"]
+        self.uri = data["uri"]
